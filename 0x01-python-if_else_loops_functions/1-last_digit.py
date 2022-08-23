55dfe80b3555f128cbddd4380def1099b7bd076a
kalbetre-alx <kalbetre@icloud.com>
2022-08-23 15:25:59 +0300

    add last digit check function

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+last_digit = abs(number) % 10
+print(f"Last digit of {number:d} is {last_digit:d} and is", end=" ")
+if last_digit == 0:
+    print(f"0")
+elif last_digit > 5:
+    print(f"greater than 5")
+else:
+    print(f"less than 6 and not 0")
