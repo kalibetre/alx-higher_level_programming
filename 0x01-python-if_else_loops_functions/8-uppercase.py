831985533aaa0b0eac9fbb212f269498a8d3165c
kalbetre-alx <kalbetre@icloud.com>
2022-08-23 16:29:46 +0300

    fix z case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,8 @@
 def uppercase(str):
     for c in str:
         print("{i:c}".format(i=ord(c) - (32 if (ord(c) >
-              96 and ord(c) < 122) else 0)), end="")
+              96 and ord(c) < 123) else 0)), end="")
     print("")
+
+
+uppercase("z")
