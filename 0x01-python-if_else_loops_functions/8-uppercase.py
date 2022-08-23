e7f7d5b880fd99c6be4070c16dd6dbb8c492af0c
kalbetre-alx <kalbetre@icloud.com>
2022-08-23 16:28:29 +0300

    print in upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        print("{i:c}".format(i=ord(c) - (32 if (ord(c) >
+              96 and ord(c) < 122) else 0)), end="")
+    print("")
