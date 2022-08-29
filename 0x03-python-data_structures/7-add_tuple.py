7ac9165f321cd842a286bae2ce7c7bb9465c7c33
kalbetre-alx <kalbetre@icloud.com>
2022-08-29 10:46:26 +0300

    remove unused import

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from typing import Tuple
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a[0] if len(tuple_a) > 0 else 0
     a += tuple_b[0] if len(tuple_b) > 0 else 0
