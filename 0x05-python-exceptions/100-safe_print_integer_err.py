f6ddab44437c98ae7594fc156cf5a5fa2d904ad4
kalbetre-alx <kalbetre@icloud.com>
2022-09-12 21:34:27 +0300

    add return true

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
+        return True
     except ValueError as tr:
         print("Exception: {}", format(tr), file=sys.stderr)
         return False
