804570cd6ccc330ede6b2e528d3c57a041311893
kalbetre-alx <kalbetre@icloud.com>
2022-09-12 21:35:18 +0300

    change expection type

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as tr:
-        print("Exception: {}", format(tr), file=sys.stderr)
+    except Exception as ex:
+        print("Exception: {}", format(ex), file=sys.stderr)
         return False
