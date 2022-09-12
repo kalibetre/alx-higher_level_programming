b2aa6243989bd84d29b3244d08bc50183e03c1fd
kalbetre-alx <kalbetre@icloud.com>
2022-09-12 20:14:35 +0300

    check for float in safe print int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
-        return True
+        if int(value) == value:
+            print("{:d}".format(int(value)))
+            return True
+        raise TypeError
     except Exception:
         return False
