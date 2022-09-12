22c298ed234869714f467afa6d599f4b9aa750d4
kalbetre-alx <kalbetre@icloud.com>
2022-09-12 22:36:42 +0300

    magic calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from dis import dis
+
+
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
+
+    return result
+
+
+dis(magic_calculation)
