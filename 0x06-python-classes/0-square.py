259be393154844925852a31d5127cea6c97aa2fd
kalbetre-alx <kalbetre@icloud.com>
2022-09-13 14:28:59 +0300

    add module doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Module 0-square
+"""Module 0-square
 
 This Module contains an definition for Square class
 """
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Module 0-square
+
+This Module contains an definition for Square class
+"""
+
+
 class Square:
     """A class that represents a square"""
 
