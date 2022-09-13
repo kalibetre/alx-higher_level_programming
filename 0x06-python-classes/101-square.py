433c0ebb8cf3ac5488cda1ce348f7df50c49ca3f
kalbetre-alx <kalbetre@icloud.com>
2022-09-13 20:22:19 +0300

    remove debuge lines

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -105,12 +105,3 @@ class Square:
                 rows.append("".join(row))
 
             return "\n".join(rows)
-
-
-my_square = Square(5, (0, 0))
-print(my_square)
-
-print("--")
-
-my_square = Square(5, (4, 1))
-print(my_square)
