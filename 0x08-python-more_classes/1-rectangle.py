85dd5ff10abf129b3e46f21912cdf6134d8d1986
kalbetre-alx <kalbetre@icloud.com>
2022-09-19 14:45:13 +0300

    add docstring and fix bug

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,15 +9,31 @@ class Rectangle:
     """A class to represent a rectangle"""
 
     def __init__(self, width=0, height=0):
+        """initializes a rectangle with width and height
+
+        Args:
+            width (int, optional): width of the rect. Defaults to 0.
+            height (int, optional): height of the rect. Defaults to 0.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """width property"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """width property setter
+
+        Args:
+            value (int): value
+
+        Raises:
+            TypeError: if width is not an int
+            ValueError: if width is negative
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -26,10 +42,20 @@ class Rectangle:
 
     @property
     def height(self):
+        """height property"""
         return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
+        """height property setter
+
+        Args:
+            value (int): value
+
+        Raises:
+            TypeError: if height is not an int
+            ValueError: if height is negative
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
