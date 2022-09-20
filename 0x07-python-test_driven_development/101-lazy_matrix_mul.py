19a8468a47208cf8d11d92d9943bb656d9d2f4e4
kalbetre-alx <kalbetre@icloud.com>
2022-09-21 01:24:16 +0300

    add lazy matrix mul

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 101-lazy_matrix_mul
+
+This Module contains an definition for lazy_matrix_mul function
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """multiplies to matrices
+    """
+    return numpy.matmul(m_a, m_b)
