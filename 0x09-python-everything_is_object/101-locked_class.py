290df1d9666e42c041ab88052c6d1c6c3ca013d0
kalbetre-alx <kalbetre@icloud.com>
2022-09-20 20:28:18 +0300

    add locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module 101-locked_class
+
+This Module contains an definition for LockedClass
+"""
+
+
+class LockedClass:
+    """A locked class that only allows the first_name attribute
+    """
+    __slots__ = "first_name"
