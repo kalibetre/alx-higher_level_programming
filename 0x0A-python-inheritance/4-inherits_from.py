55589abedbbc7ac391a18291c15292c0b12a888b
kalbetre-alx <kalbetre@icloud.com>
2022-09-26 15:25:52 +0300

    add inhertis_from function

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module 4-inherits_from
+
+This Module contains a definition for inherits_from function
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    checks if obj is subclass of type a_class
+
+    Args:
+        obj: the object
+        a_class: expected type
+
+    Returns: True if it is otherwise False
+    """
+    return issubclass(obj, a_class)
