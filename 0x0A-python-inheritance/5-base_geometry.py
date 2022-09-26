55aa75257d9ba2c1a08d15e27017ef7be12d114b
kalbetre-alx <kalbetre@icloud.com>
2022-09-26 15:30:40 +0300

    empty base geometry class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 5-base_geometry
+
+This Module contains a definition for BaseGeometry Class
+"""
+
+
+class BaseGeometry:
+    """A class that is parent of all geometries"""
+    pass
