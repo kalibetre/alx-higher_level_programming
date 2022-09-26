7bf1b92a926164a920f6c244ebd413a725514d36
kalbetre-alx <kalbetre@icloud.com>
2022-09-26 17:23:29 +0300

    add area and __str__ for Rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module 9-rectangle
+
+This Module contains a definition for Rectangle Class
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle subclass of BaseGeometry"""
+
+    def __init__(self, width, height):
+        """initializes Rectangle class
+
+        Args:
+            width (int): width of the rect
+            height (int): height of the rect
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self) -> str:
+        return f"[Rectangle] {self.__width}/{self.__height}"
