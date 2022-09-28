1fee80acad0e5d5c97205a7eaf81d3ad74c08da5
kalbetre-alx <kalbetre@icloud.com>
2022-09-29 00:41:58 +0300

    refactor and print only valid lines

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,39 +14,36 @@ regex = r"((?:\d{1,3}\.{0,1}){4}) - \[(.+)\] \"(.+)\" (\d{3}) (\d+)"
 
 def main():
     """main script function to read input from stdin and process it"""
-    chunk = []
+    line_count = 0
     er_codes = dict()
     file_size = 0
 
     try:
         for line in sys.stdin:
-            chunk.append(line)
-            if len(chunk) == 10:
-                file_size, er_codes = print_metrics(
-                    chunk, file_size, er_codes)
-                chunk.clear()
+            matches = re.finditer(regex, line)
+            match = next(matches, None)
+            if match is not None:
+                er_codes[match.group(4)] = er_codes.get(match.group(4), 0) + 1
+                file_size += int(match.group(5))
+                line_count += 1
+                if line_count % 10 == 0:
+                    print_metrics(file_size, er_codes)
+
     except KeyboardInterrupt:
-        print_metrics(chunk, file_size, er_codes)
+        print_metrics(file_size, er_codes)
         raise
 
-    print_metrics(chunk, file_size, er_codes)
+    print_metrics(file_size, er_codes)
 
 
-def print_metrics(chunk, file_size, er_codes):
+def print_metrics(file_size, er_codes):
     """
     computes the matrices of a set of 10 inputs or whatever is left after
     signal interrupt
     """
-    for line in chunk:
-        matches = re.finditer(regex, line)
-        match = next(matches, None)
-        er_codes[match.group(4)] = er_codes.get(match.group(4), 0) + 1
-        file_size += int(match.group(5))
     print(f"File size: {file_size}")
     for code in sorted(er_codes.items(), key=lambda x: x[0]):
         print(f"{code[0]}: {code[1]}")
-
-    return file_size, er_codes
 
 
 if __name__ == "__main__":
