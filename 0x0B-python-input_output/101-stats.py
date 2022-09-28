68eb3d1776ff914dd3c1a9e4ec5116f18ee1619b
kalbetre-alx <kalbetre@icloud.com>
2022-09-29 00:33:39 +0300

    resitric status codes to given ones

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -15,7 +15,8 @@ regex = r"((?:\d{1,3}\.{0,1}){4}) - \[(.+)\] \"(.+)\" (\d{3}) (\d+)"
 def main():
     """main script function to read input from stdin and process it"""
     chunk = []
-    er_codes = dict()
+    er_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
     file_size = 0
 
     try:
@@ -40,11 +41,13 @@ def print_metrics(chunk, file_size, er_codes):
     for line in chunk:
         matches = re.finditer(regex, line)
         match = next(matches, None)
-        er_codes[match.group(4)] = er_codes.get(match.group(4), 0) + 1
+        if match.group(4) in er_codes:
+            er_codes[match.group(4)] += 1
         file_size += int(match.group(5))
     print(f"File size: {file_size}")
     for code in sorted(er_codes.items(), key=lambda x: x[0]):
-        print(f"{code[0]}: {code[1]}")
+        if int(code[1]) > 0:
+            print(f"{code[0]}: {code[1]}")
 
     return file_size, er_codes
 
