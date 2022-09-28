86cd9095fe1abf8785daadc1f5a98f48761decfc
kalbetre-alx <kalbetre@icloud.com>
2022-09-28 13:20:21 +0300

    add docstring for save to json function

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """saves the json representation of the object to file"""
     with open(filename, "w") as f:
         f.write(json.dumps(my_obj))
