21210aa578451ba786543caeca58147cdce7eb62
kalbetre-alx <kalbetre@icloud.com>
2022-09-28 23:53:16 +0300

    refactor stats to be since the begining

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -15,25 +15,26 @@ regex = r"((?:\d{1,3}\.{0,1}){4}) - \[(.+)\] \"(.+)\" (\d{3}) (\d+)"
 def main():
     """main script function to read input from stdin and process it"""
     chunk = []
+    er_codes = dict()
+    file_size = 0
     while True:
         try:
             for line in sys.stdin:
                 chunk.append(line)
                 if len(chunk) == 10:
-                    print_metrics(chunk)
+                    file_size, er_codes = print_metrics(
+                        chunk, file_size, er_codes)
                     chunk.clear()
         except KeyboardInterrupt:
-            print_metrics(chunk)
+            print_metrics(chunk, file_size, er_codes)
             sys.exit(1)
 
 
-def print_metrics(chunk):
+def print_metrics(chunk, file_size, er_codes):
     """
     computes the matrices of a set of 10 inputs or whatever is left after
     signal interrupt
     """
-    file_size = 0
-    er_codes = dict()
     for line in chunk:
         matches = re.finditer(regex, line)
         match = next(matches, None)
@@ -42,6 +43,8 @@ def print_metrics(chunk):
     print(f"File size: {file_size}")
     for code in sorted(er_codes.items(), key=lambda x: x[0]):
         print(f"{code[0]}: {code[1]}")
+
+    return file_size, er_codes
 
 
 if __name__ == "__main__":
