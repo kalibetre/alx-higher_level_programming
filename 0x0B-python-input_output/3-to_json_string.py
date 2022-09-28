a702b2880d1a2b6d4d1fa1136cd1a9b669d0af4b
kalbetre-alx <kalbetre@icloud.com>
2022-09-28 13:00:07 +0300

    add json str function

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 3-to_json_string 
+
+This Module contains a definition for to_json_string function
+"""
+import json
+
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
