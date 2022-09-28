c198ea0bd76f3847a3695b898c80492a85638c5f
kalbetre-alx <kalbetre@icloud.com>
2022-09-28 12:47:25 +0300

    remove test func call

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -11,6 +11,3 @@ def write_file(filename="", text=""):
     with open(filename, "w", encoding="utf-8") as f:
         bytes_wrt = f.write(text)
     return bytes_wrt
-
-
-write_file("temp.txt", "test string")
