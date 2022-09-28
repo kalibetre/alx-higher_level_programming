6ba1d0463671e051823c4da7d1523c0d4c9f484d
kalbetre-alx <kalbetre@icloud.com>
2022-09-28 12:49:48 +0300

    add appendfile function

---
+++ 0x0B-python-input_output/2-append_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module 2-append_file
+
+This Module contains a definition for append_file function
+"""
+
+
+def append_file(filename="", text=""):
+    """appends a text to file encoded with utf-8"""
+    bytes_wrt = 0
+    with open(filename, "a", encoding="utf-8") as f:
+        bytes_wrt = f.write(text)
+    return bytes_wrt
