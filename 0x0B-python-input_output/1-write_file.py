256e1ed401c7dcb0f362e5c1d3e29e3847ac4e47
kalbetre-alx <kalbetre@icloud.com>
2022-09-28 12:46:51 +0300

    add writefile function

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module 1-write_file
+
+This Module contains a definition for write_file function
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a text to file encoded with utf-8"""
+    bytes_wrt = 0
+    with open(filename, "w", encoding="utf-8") as f:
+        bytes_wrt = f.write(text)
+    return bytes_wrt
+
+
+write_file("temp.txt", "test string")
