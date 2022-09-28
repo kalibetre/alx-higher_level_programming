bd155d25958a344888827919db89cfd72d1464c9
kalbetre-alx <kalbetre@icloud.com>
2022-09-28 16:33:01 +0300

    json update file script

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module 7-add_item
+
+This Module contains a definition for add_item function
+"""
+
+from os import path
+from sys import argv
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def main():
+    """reads a list from file and appends the arguments to it"""
+    filename = "add_item.json"
+    data = list(load_from_json_file(filename)) if path.exists(filename) else []
+    for i in range(1, len(argv)):
+        data.append(argv[i])
+    save_to_json_file(data, filename)
+
+
+if __name__ == "__main__":
+    main()
