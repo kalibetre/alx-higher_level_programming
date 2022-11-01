850048181d959a8f0574591767568190ac502c4c
kalbetre-alx <kalbetre@icloud.com>
2022-11-01 23:56:46 +0300

    sql to change charset to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,14 @@
+-- convert database and tables to utf8mb4
+ALTER DATABASE hbtn_0c_0
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+	CHANGE name name
+	varchar(256)
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
