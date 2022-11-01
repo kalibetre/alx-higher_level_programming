87bd8a70a730d35e285ed615be11281e3a678ed8
kalbetre-alx <kalbetre@icloud.com>
2022-11-02 00:03:39 +0300

    remove col change

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,9 +8,3 @@ USE hbtn_0c_0;
 ALTER TABLE first_table
 	CONVERT TO CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table
-	CHANGE name name
-	varchar(256)
-	CHARACTER SET utf8mb4
-	COLLATE utf8mb4_unicode_ci;
