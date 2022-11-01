2aa80d5c0e4115aa9e0492eb39d7fb48347b74ec
kalbetre-alx <kalbetre@icloud.com>
2022-11-01 23:59:02 +0300

    fix convert to

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,7 +4,7 @@ ALTER DATABASE hbtn_0c_0
 	COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
-	CHARACTER SET utf8mb4
+	CONVERT TO CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
