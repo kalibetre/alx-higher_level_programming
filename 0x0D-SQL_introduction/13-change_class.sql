92b8a380f7a635b256d3849c2979c381f89cdb7c
kalbetre-alx <kalbetre@icloud.com>
2022-11-01 23:34:35 +0300

    sql to remove low scores and compute avg

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- deletes records with score <= 5 from the second_table
+DELETE second_table
+WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- computes the average of scores in second_table
+SELECT AVG(score)
+FROM second_table;
