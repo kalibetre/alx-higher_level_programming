8c191d88a04ab7d61e1d13ea5bdeb9b3208af050
kalbetre-alx <kalbetre@icloud.com>
2022-11-03 00:31:39 +0300

    not in subquery for titles

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- list all all shows without the genre Comedy
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN (
+	SELECT tv_shows.title
+	FROM tv_shows
+	JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY tv_shows.title ASC;
