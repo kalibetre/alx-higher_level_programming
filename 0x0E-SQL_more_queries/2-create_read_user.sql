8b626e040491ebaa55fe588b137d0db31e608076
kalbetre-alx <kalbetre@icloud.com>
2022-11-02 22:11:31 +0300

    sql to create a read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creates a new database and user
+CREATE DATABASE IF NOT EXIST hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS user_0d_2@localhost
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO user_0d_1@localhost;
