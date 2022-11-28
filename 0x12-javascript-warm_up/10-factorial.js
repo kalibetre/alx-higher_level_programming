1fd8cb3c3b9e7119f61771222b07ed94d0c7af19
kalibetre <kalbetre@icloud.com>
2022-11-28 23:36:46 +0300

    handle negative numbers

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 function factorial (num) {
+  if (isNaN(num) || num === 0) return 1;
+  else if (num < 0) return NaN;
+
   let fact = 1;
   if (!isNaN(num)) {
     while (num > 1) fact *= num--;
