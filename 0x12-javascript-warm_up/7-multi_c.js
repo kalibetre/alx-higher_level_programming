ff97bd9173e56768a69e2a8f489c9b78744c14f7
kalibetre <kalbetre@icloud.com>
2022-11-28 23:11:56 +0300

    bug fix for negative values

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,5 +2,5 @@
 let count = Number.parseInt(process.argv[2]);
 if (Number.isNaN(count)) console.log('Missing number of occurrences');
 else {
-  while (count--) console.log('C is fun');
+  while (count-- > 0) console.log('C is fun');
 }
