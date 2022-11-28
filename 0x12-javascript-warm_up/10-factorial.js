4c9ace9b3aea64c91f66a45e65d8fc28942b9e34
kalibetre <kalbetre@icloud.com>
2022-11-28 23:32:52 +0300

    add base to parseInt

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 function factorial (num) {
   let fact = 1;
-  if (!Number.isNaN(num)) {
+  if (!isNaN(num)) {
     while (num > 1) fact *= num--;
   }
-  console.log(fact);
+  return fact;
 }
-const num = Number.parseInt(process.argv[2]);
-factorial(num);
+
+console.log(factorial(parseInt(process.argv[2], 10)));
