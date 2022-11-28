669a6a236e4157069d6903d4a24c8b1ff8c819dd
kalibetre <kalbetre@icloud.com>
2022-11-28 23:45:53 +0300

    use recurssion

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,7 @@
 function factorial (num) {
   if (isNaN(num) || num === 0) return 1;
   else if (num < 0) return NaN;
-
-  let fact = 1;
-  if (!isNaN(num)) {
-    while (num > 1) fact *= num--;
-  }
-  return fact;
+  return num * factorial(num - 1);
 }
 
 console.log(factorial(parseInt(process.argv[2], 10)));
