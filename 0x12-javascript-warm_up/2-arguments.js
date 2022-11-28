bf07d8e2430751548bb047538067b8c4c7fa41fa
kalibetre <kalbetre@icloud.com>
2022-11-28 22:15:45 +0300

    fix arg number

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const len = process.argv.length;
-if (len === 0) console.log('No argument');
-else if (len === 1) console.log('Argument found');
+if (len === 2) console.log('No argument');
+else if (len === 3) console.log('Argument found');
 else console.log('Arguments found');
