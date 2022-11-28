9f0bdb04091e6d30677d276aca0928866fb88d8e
kalibetre <kalbetre@icloud.com>
2022-11-29 00:46:43 +0300

    provide comparator for sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 function findSecondBiggest (values) {
-  if (values.length === 0 || values.length === 1) return 0;
-  return values.sort().reverse()[1];
+  if (values.length <= 1) return 0;
+  return values.sort((a, b) => a - b).reverse()[1];
 }
 
 const nums = process.argv.slice(2).map((x) => parseInt(x, 10));
