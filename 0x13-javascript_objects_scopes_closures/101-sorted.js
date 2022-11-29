003b710742f7e4f29ee6659549d84996fdbba868
kalibetre <kalbetre@icloud.com>
2022-11-29 17:29:09 +0300

    add a script to summerize an obj by values

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+function summarizeByValues (objDict) {
+  const entries = Object.entries(objDict);
+  const newDict = {};
+  for (const entry of entries) {
+    newDict[entry[1]] = entries
+      .filter((value) => value[1] === entry[1])
+      .map((value) => value[0]);
+  }
+  return newDict;
+}
+
+console.log(summarizeByValues(dict));
