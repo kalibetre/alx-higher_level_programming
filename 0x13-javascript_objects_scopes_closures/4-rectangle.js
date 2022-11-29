eb1a7929ea06fd166bbcbddd01fd9bbc8d609dde
kalibetre <kalbetre@icloud.com>
2022-11-29 15:14:43 +0300

    add rotate and double methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(
+      'X'.repeat(this.width).concat('\n').repeat(this.height).trimEnd('\n')
+    );
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+}
+
+module.exports = Rectangle;
