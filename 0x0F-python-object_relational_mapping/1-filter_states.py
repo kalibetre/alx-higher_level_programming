a7f7a71c8a3f0a9b3505845b5e7b875939be7333
kalibetre <kalbetre@icloud.com>
2022-12-03 21:26:54 +0300

    add orderby to the filted state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -25,7 +25,7 @@ def list_all_states_filtered(username, password, db_name):
         charset="utf8"
     )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
