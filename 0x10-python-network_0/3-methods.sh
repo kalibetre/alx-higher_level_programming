da575a0058dca7ea51959f6daef2d2787bf10f3c
kalibetre <kalbetre@icloud.com>
2022-12-15 22:01:48 +0300

    remove options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script that sends a request to a URL and displays content length
-curl -si -X OPTIONS "$1" | grep -i Allow | cut -d ":" -f 2 | awk '{$1=$1};1'
+curl -sI "$1" | grep -i Allow | cut -d ":" -f 2 | awk '{$1=$1};1'
