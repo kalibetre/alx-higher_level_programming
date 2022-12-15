a69004d4616b9cb2d36285d1a8aaf9b99cbf4bd7
kalibetre <kalbetre@icloud.com>
2022-12-15 21:26:52 +0300

    curl to get content length

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# A bash script that
+# 	- takes in a URL
+#	- sends a request to the URL and 
+#	- displays the size of the body of the response
+
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
