ebf67da142f31ae970a9cf814ff4034b61746b70
kalibetre <kalbetre@icloud.com>
2022-12-16 19:37:32 +0300

    read header with requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """5-hbtn_header module"""
+from sys import argv
+
 import requests
 
 if __name__ == "__main__":
-    resp = requests.get("https://alx-intranet.hbtn.io/status")
+    resp = requests.get(argv[1])
     print(resp.headers.get("X-Request-Id"))
