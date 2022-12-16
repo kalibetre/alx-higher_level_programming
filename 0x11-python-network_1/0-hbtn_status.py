95358b20cc7bc3a61981eba3d7745d4bd51e32f7
kalibetre <kalbetre@icloud.com>
2022-12-16 18:38:10 +0300

    refactor script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 """0-hbtn_status module"""
 
-import urllib.request
+if __name__ == "__main__":
+    from urllib import request
 
-
-def fetch_hbtn():
-    """fetches the hbtn intranet and displays information
-    """
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as r:
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
         html = r.read()
         print("Body response:")
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
         print(f"\t- utf8 content: {html.decode('utf-8')}")
-
-
-if __name__ == "__main__":
-    fetch_hbtn()
