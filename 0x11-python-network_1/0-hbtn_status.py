500fa0b5f61429df520773e7eaa428818a35383e
kalibetre <kalbetre@icloud.com>
2022-12-16 18:28:14 +0300

    use space instead of tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,14 @@ import urllib.request
 
 
 def fetch_hbtn():
+    """fetches the hbtn intranet and displays information
+    """
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as r:
         html = r.read()
         print("Body response:")
-        print(f"\t- type: {type(html)}")
-        print(f"\t- content: {html}")
-        print(f"\t- utf8 content: {html.decode('utf-8')}")
+        print(f"    - type: {type(html)}")
+        print(f"    - content: {html}")
+        print(f"    - utf8 content: {html.decode('utf-8')}")
 
 
 if __name__ == "__main__":
