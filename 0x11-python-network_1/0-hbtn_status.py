2a9123671baf4aa84ffa59de2ac1904ec53316ba
kalibetre <kalbetre@icloud.com>
2022-12-16 18:24:33 +0300

    fix formating

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,9 @@ def fetch_hbtn():
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as r:
         html = r.read()
         print("Body response:")
-        print(f"\t- type:{type(html)}")
-        print(f"\t- content:{html}")
-        print(f"\t- utf8 content:{html.decode('utf-8')}")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
 
 
 if __name__ == "__main__":
