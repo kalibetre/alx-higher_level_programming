237017133411ca64e5ca699aba7c17d14ba057d6
kalibetre <kalbetre@icloud.com>
2022-12-16 19:54:37 +0300

    handle json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""7-error_code module"""
+from sys import argv
+
+import requests
+
+if __name__ == "__main__":
+    data = {"q": argv[1] if len(argv) > 1 else ""}
+    resp = requests.post("http://0.0.0.0:5000/search_user", data=data)
+
+    try:
+        js_data = resp.json()
+
+        if js_data:
+            id = js_data.get("id")
+            name = js_data.get("name")
+            print(f"[{id}] {name}")
+        else:
+            print("No result")
+    except Exception as e:
+        print("Not a valid JSON")
