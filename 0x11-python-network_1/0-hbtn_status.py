f480ad94cbff7e27aa9074959d344028709a3512
kalibetre <kalbetre@icloud.com>
2022-12-16 18:46:29 +0300

    check 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""0-hbtn_status module"""
+"""
+fetch https://alx-intranet.hbtn.io/status display response
+"""
+
+import urllib.request
 
 if __name__ == "__main__":
-    import urllib.request
-
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as r:
         html = r.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
