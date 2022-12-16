b2c38fd40b15b16bb7598a8edede03a13b568c88
kalibetre <kalbetre@icloud.com>
2022-12-16 19:01:13 +0300

    use tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,9 +10,9 @@ def fetch_hbtn():
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as r:
         html = r.read()
         print("Body response:")
-        print(f"    - type: {type(html)}")
-        print(f"    - content: {html}")
-        print(f"    - utf8 content: {html.decode('utf-8')}")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf-8')}")
 
 
 if __name__ == "__main__":
